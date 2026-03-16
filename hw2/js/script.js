33b51c7f2d9a57b5691b5fe884bb6577ba171299
Mariah Stinson <mstinson@csumb.edu>
2026-03-15 21:51:20 -0700

    hw2 after completing step 5

--- hw2/js/script.js
+++ hw2/js/script.js
@@ -1,7 +1,21 @@
 document.querySelector("button").addEventListener("click", gradeQuiz);
 
+// Functions
+function isFormValid() {
+    let isValid = true;
+    if(document.querySelector("#q1").value == ""){
+        isValid = false;
+        document.querySelector("#validationFdbk").innerHTML = "Question 1 was not answered.";
+    }
+    return isValid;
+}
+
 function gradeQuiz() {
     console.log("Grading quiz...");
+    document.querySelector("#validationFdbk").innerHTML = ""; //resets validation fdbk
+    if (!isFormValid()) {
+        return;
+    }
     let q1Response = document.querySelector("#q1").value;
     console.log(q1Response);
 }
