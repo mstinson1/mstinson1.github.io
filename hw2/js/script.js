b591db8a7b4f8322e147370a030114da7cbc1cda
Mariah Stinson <mstinson@csumb.edu>
2026-03-16 14:03:25 -0700

    HW2 after completing step 6

--- hw2/js/script.js
+++ hw2/js/script.js
@@ -16,6 +16,22 @@ function gradeQuiz() {
     if (!isFormValid()) {
         return;
     }
-    let q1Response = document.querySelector("#q1").value;
+
+    // variables
+    let score = 0;
+    let q1Response = document.querySelector("#q1").value.toLowerCase();
     console.log(q1Response);
+
+    // grading question 1
+    if (q1Response == "sacramento") {
+        document.querySelector("#q1Feedback").innerHTML = "Correct!";
+        document.querySelector("#q1Feedback").className = "bg-success text-white";
+        document.querySelector("#markImg1").innerHTML = "<img src='img/checkmark.png' alt='Checkmark'>";
+        score += 20;
+    } else {
+        document.querySelector("#q1Feedback").innerHTML = "Incorrect!";
+        document.querySelector("#q1Feedback").className = "bg-warning text-white";
+        document.querySelector("#markImg1").innerHTML = "<img src='img/xmark.png' alt='xmark'>";
+    }
+    document.querySelector("#totalScore").innerHTML = `Total Score: ${score}`;
 }
