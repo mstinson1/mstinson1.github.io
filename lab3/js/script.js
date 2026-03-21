3835e2b04a98f2bf3a4d785df12a779f6dcaac85
Mariah Stinson <mstinson@csumb.edu>
2026-03-21 10:50:17 -0700

    Lab3 after completion of lesson 3

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -1,5 +1,6 @@
 // event listeners
 document.querySelector("#zip").addEventListener("change", displayCity);
+document.querySelector("#state").addEventListener("change", displayCounties);
 
 // functions
 
@@ -15,4 +16,17 @@ async function displayCity() {
     document.querySelector("#city").innerHTML = data.city;
     document.querySelector("#latitude").innerHTML = data.latitude;
     document.querySelector("#longitude").innerHTML = data.longitude;
+}
+
+// Document counties from Web API based on the two-letter abbreviation of a state
+async function displayCounties() {
+    let state = document.querySelector("#state").value;
+    let url = `https://csumb.space/api/countyListAPI.php?state=${state}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let countyList = document.querySelector("#county");
+    countyList.innerHTML = "<option> Select County </option>";
+    for (let i = 0; i < data.length; i++) {
+        countyList.innerHTML += `<option> ${data[i].county} </option>`;
+    }
 }
